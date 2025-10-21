788de2ace1fe19922de346d41b237e6d667afc1c
larrycoder123 <michaelboesch.study@gmail.com>
2025-10-21 15:37:09 +0200

    add frontend url to allowed for api

--- app/main.py
+++ app/main.py
@@ -24,6 +24,7 @@ def create_app() -> FastAPI:
             "http://localhost:5173",
             "http://localhost:8080",
             "http://127.0.0.1:8080",
+            "https://motify.vercel.app",
         ],
         allow_credentials=True,
         allow_methods=["*"],
